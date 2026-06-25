d67d2694a4a9e47c2be8378e9a977319875b80b5
GundamSneed <292934846+GundamSneed@users.noreply.github.com>
2026-06-24 20:03:47 -0400

    REPEAT STRING FUNCTION COMPLETED FILES
    Uploading the completed script for 06, repeatString function practice assignment. All tests passed.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
 
-};
+    if (times < 0) {
+        return 'ERROR';
+    }
+
+    let out = "";
+    for (let i = 0; i < times; i++) {
+        
+        out += word;
+
+    }
+    return out;
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
